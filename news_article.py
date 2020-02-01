457279707a186db5f1da1dc8ec7f38d9d2c8371a
pumpkinpie94 <49765281>
2020-02-01 12:36:26 -0500

    version 1.0 done and works

--- news_article.py
+++ news_article.py
@@ -2,12 +2,47 @@
 from bs4 import BeautifulSoup
 import requests
 
-source = requests.get('https://www.nytimes.com/2018/11/11/business/intelligence-expert-wall-street.html').text
+#path and document extension
+path = "B:/articles/"
+fileType = '.txt'
 
+#get url from user
+url = input("url of article:")
+
+#supply url to requests to get html
+source = requests.get(url).text
+
+#give html to beautifulsoup to parse
 soup = BeautifulSoup(source, 'lxml')
 
-for text in soup.find_all('p'):
-    print(text.text)
-    print()
+# attempt to get article title
+realTitle = soup.find('span', class_='ghost')
 
+#if title found use as document name and add to top of document
+if(realTitle != None):
+    #name and path of document
+    title = realTitle.text
+
+    #create document in specified path
+    file = open(path+title+fileType, 'w')
+    file.write(realTitle.text)
+    file.write('\n\n\n')
+
+else:
+    #name and path of document
+    print("Sorry, I was not able to find the title of the article. Please supply one.")
+    title = input("Title:")
+
+    #create document in specified path
+    file = open(path+title+fileType, 'w')
+
+
+
+
+for text in soup.find_all('p'):
+    file.write(text.text)
+    file.write("\n\n")
+
+file.close
+print("done writing file")
 #print(soup.prettify())
