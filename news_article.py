38c7ed2268807b789b156c7b4560680150c5f333
pumpkinpie94 <49765281>
2020-02-01 11:51:33 -0500

    updated to use BeautifulSoup

--- news_article.py
+++ news_article.py
@@ -1,17 +1,13 @@
 ## script to download news articles from page html, created by pumpkinpie94 on 1/25/2020
-import urllib.request
-import urllib.error
-import urllib.parse
+from bs4 import BeautifulSoup
+import requests
 
-url = 'https://www.theatlantic.com/ideas/archive/2020/01/american-housing-has-gone-insane/605005/?utm_source=pocket-newtab'
-title = 'american housing has gone insane'
-path = 'B:/articles/'
-fileType = '.txt'
-file = open(path+title+fileType, 'w')
+source = requests.get('https://www.nytimes.com/2018/11/11/business/intelligence-expert-wall-street.html').text
 
+soup = BeautifulSoup(source, 'lxml')
 
-response = urllib.request.urlopen(url)
-webContent = response.read()
-file.write(webContent.decode('utf-8'))
-file.close()
-print(webContent)
+for text in soup.find_all('p'):
+    print(text.text)
+    print()
+
+#print(soup.prettify())
